808db1f72ea6e181f0d3a1e860f49c2589408266
Archita <architagupta1173@gmail.com>
2026-03-01 21:57:23 -0800

    frontend addes register and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {RouterProvider} from "react-router-dom"
+import {router} from "./app.routes.jsx"
 
 const App = () => {
   return (
     <div>
-      
+      <RouterProvider router={router} />
     </div>
   )
 }
